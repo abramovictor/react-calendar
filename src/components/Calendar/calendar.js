165e1091cd6a92515f860e09a3f42ae50b50d823
adaltezze <adaltezze@gmail.com>
2019-04-12 02:19:48 +0500

    add logic |  create data

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -1,9 +1,37 @@
+const DAYS_IN_WEEK = 7;
+const WEEK_IN_MONTH = 6;
+
+export function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+}
+
+export function getDayOfWeek(date) {
+    const dayOfWeek = date.getDay();
+    return dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+}
+
+
 export function getMonthData(year, month) {
-    return [
-        [undefined, undefined, new Date(), new Date(), new Date(), new Date(), new Date()],
-        [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
-        [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
-        [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
-        [new Date(), new Date(), new Date(), new Date(), undefined, undefined, undefined],
-    ];
+    const result = [];
+    const date = new Date(year, month);
+    const monthStartOn = getDayOfWeek(date);
+    const daysInMonth = getDaysInMonth(year, month);
+
+    let currentMonthDay = 1;
+    let prevMonthDay = (daysInMonth - monthStartOn) + 1;
+
+    for (let cellWeek = 0; cellWeek < WEEK_IN_MONTH; cellWeek++) {
+        result[cellWeek] = [];
+        
+        for (let cellDay = 0; cellDay < DAYS_IN_WEEK; cellDay++) {
+            if (cellWeek === 0 && cellDay < monthStartOn) {
+                result[cellWeek][cellDay] = new Date(year, month - 1, prevMonthDay++);
+            }
+            else {
+                result[cellWeek][cellDay] = new Date(year, month, currentMonthDay++);
+            }
+        }
+    }
+
+    return result;
 }
