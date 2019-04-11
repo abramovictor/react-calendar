69b97670614bf5a99b4da7a24166a945f17d0165
adaltezze <adaltezze@gmail.com>
2019-04-11 23:41:50 +0500

    add controls |  table

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import Calendar from './components/Calendar';
 
+
 class App extends Component {
     render() {
         return (
             <div className="container">
-                <div className="row">
-                    <div className="col-xl-6 col-lg-8 col-md-10 justify-content-center">
+                <div className="row justify-content-center pt-5">
+                    <div className="col-xl-6 col-lg-8 col-md-10 pt-5">
                         <Calendar />
                     </div>
                 </div>
             </div>
-     );
+        );
     }
 }
 
--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -3,7 +3,33 @@ import React, { Component } from 'react';
 export default class Calendar extends Component {
     render() {
         return (
+            <div className="calendar card bg-dark shadow">
+                <header className="card-header d-flex">
+                    <div className="btn-group mr-3">
+                        <button className="btn btn-light">
+                            ❮
+                        </button>
+                        <button className="btn btn-light">
+                            ❯
+                        </button>
+                    </div>
 
+                    <div className="input-group">
+                        <select className="form-control"></select>
+                        <select className="form-control"></select>
+                    </div>
+                </header>
+                <section className="card-body">
+                    <table className="table">
+                        <thead>
+                            <tr></tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </section>
+            </div>
         );
     }
 }
