dd0303ab10f408386121549af1a1e89a24d8ad21
adaltezze <adaltezze@gmail.com>
2019-04-12 00:57:36 +0500

    add render | handlers

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,31 +1,151 @@
 import React, { Component } from 'react';
 
 export default class Calendar extends Component {
+    static defaultProps = {
+        onChange: Function.prototype,
+
+        date: new Date(),
+        years: ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        buttonMonthNames: {
+            next: 'Следующий месяц',
+            prev: 'Предыдущий месяц',
+        },
+        weekDayNames: [
+            {
+                short: 'Пн',
+                full: 'Понедельник'
+            },
+            {
+                short: 'Вт',
+                full: 'Вторник'
+            },
+            {
+                short: 'Ср',
+                full: 'Среда'
+            },
+            {
+                short: 'Чт',
+                full: 'Четверг'
+            },
+            {
+                short: 'Пт',
+                full: 'Пятница'
+            },
+            {
+                short: 'Сб',
+                full: 'Суббота'
+            },
+            {
+                short: 'Вс',
+                full: 'Воскресенье'
+            }
+        ]
+    };
+
+    state = {
+        date: this.props.date,
+        currentDate: new Date(),
+        selectedDate: null
+    };
+
+    get year() {
+        return this.state.date.getFullYear();
+    };
+
+    get month() {
+        return this.state.date.getMonth();
+    };
+
+    get day() {
+        return this.state.date.getDate();
+    };
+
+    handlePrevMonthButtonClick = () => {
+        const date = new Date(this.year, this.month - 1);
+        this.setState({ date });
+    };
+
+    handleNextMonthButtonClick = () => {
+        const date = new Date(this.year, this.month + 1);
+        this.setState({ date });
+    };
+
+    handleSelectChange = () => { };
+
+    handledayClick = (date) => {
+        console.log(date);
+
+        this.setState({ selectedDate: date });
+        this.props.onChange(date);
+    };
+
     render() {
+        const { years, monthNames, weekDayNames } = this.props;
+
+        const monthData = [
+            [undefined, undefined, new Date(), new Date(), new Date(), new Date(), new Date()],
+            [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
+            [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
+            [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
+            [new Date(), new Date(), new Date(), new Date(), undefined, undefined, undefined],
+        ];
+
         return (
             <div className="calendar card bg-dark shadow">
                 <header className="card-header d-flex">
                     <div className="btn-group mr-3">
-                        <button className="btn btn-light">
-                            ❮
-                        </button>
-                        <button className="btn btn-light">
-                            ❯
-                        </button>
+                        <button
+                            className="btn btn-light"
+                            title={this.props.buttonMonthNames.prev}
+                            onClick={() => this.handlePrevMonthButtonClick()}>
+
+                            {'❮'}</button>
+                        <button
+                            className="btn btn-light"
+                            title={this.props.buttonMonthNames.next}
+                            onClick={() => this.handleNextMonthButtonClick()}>
+
+                            {'❯'}</button>
                     </div>
 
                     <div className="input-group">
-                        <select className="form-control"></select>
-                        <select className="form-control"></select>
+                        <select className="form-control btn-light">
+                            {monthNames.map((name, index) =>
+                                <option key={name} value={index}>{name}</option>
+                            )}
+                        </select>
+                        <select className="form-control btn-light">
+                            {years.map(year =>
+                                <option key={year} value={year}>{year}</option>
+                            )}
+                        </select>
                     </div>
                 </header>
                 <section className="card-body">
-                    <table className="table">
+                    <table className="table table-bordered table-dark text-center">
                         <thead>
-                            <tr></tr>
+                            <tr>
+                                {weekDayNames.map(({ short: name }) =>
+                                    <th key={name}>{name}</th>
+                                )}
+                            </tr>
                         </thead>
                         <tbody>
+                            {monthData.map((week, index) =>
+                                <tr key={index}>
+                                    {week.map((date, index) => date ?
+                                        <td className="p-1" key={index}>
+                                            <button
+                                                className="btn btn-dark h-100 w-100"
+                                                onClick={() => this.handledayClick(date)}>
 
+                                                {date.getDate()}</button>
+                                        </td> :
+                                        <td key={index} />
+                                    )}
+                                </tr>
+                            )}
                         </tbody>
                     </table>
                 </section>
