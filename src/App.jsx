ce2399b623f34f69ebf5317a94969a33e36d9e01
adaltezze <adaltezze@gmail.com>
2021-03-16 12:45:33 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,30 @@ import React, { Component } from 'react';
 import Calendar from './components/Calendar';
 
 class App extends Component {
-    state = {
-        date: null
-    };
+  state = {
+    date: null
+  };
 
-    handleDateChange = date => this.setState({ date });
+  handleDateChange = date => this.setState({ date });
 
-    render() {
-        let date = this.state.date ? this.state.date : new Date();
+  render() {
+    let date = this.state.date ? this.state.date : new Date();
 
-        return (
-            <div className="container">
-                <div className="row justify-content-center pt-5">
-                    <div className="col-xl-6 col-lg-8 col-md-10 pt-5">
-                        <div className="card bg-dark text-light rounded-0">
-                            <header className="card-header border-bottom-0 rounded-0"><strong>Выбранная дата:</strong> {date.toLocaleDateString()}</header>
-                        </div>
-                        <Calendar
-                            onChange={this.handleDateChange}
-                        />
-                    </div>
-                </div>
+    return (
+      <div className="container">
+        <div className="row justify-content-center pt-5">
+          <div className="col-xl-6 col-lg-8 col-md-10 pt-5">
+            <div className="card bg-dark text-light rounded-0">
+              <header className="card-header border-bottom-0 rounded-0"><strong>Выбранная дата:</strong> {date.toLocaleDateString()}</header>
             </div>
-        );
-    }
+            <Calendar
+              onChange={this.handleDateChange}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -3,171 +3,169 @@ import classnames from 'classnames';
 import * as calendar from './calendar';
 
 export default class Calendar extends Component {
-    static defaultProps = {
-        onChange: Function.prototype,
+  static defaultProps = {
+    onChange: Function.prototype,
 
-        date: new Date(),
-        years: ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
-        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-        buttonMonthNames: {
-            next: 'Следующий месяц',
-            prev: 'Предыдущий месяц',
-        },
-        weekDayNames: [
-            {
-                short: 'Пн',
-                full: 'Понедельник'
-            },
-            {
-                short: 'Вт',
-                full: 'Вторник'
-            },
-            {
-                short: 'Ср',
-                full: 'Среда'
-            },
-            {
-                short: 'Чт',
-                full: 'Четверг'
-            },
-            {
-                short: 'Пт',
-                full: 'Пятница'
-            },
-            {
-                short: 'Сб',
-                full: 'Суббота'
-            },
-            {
-                short: 'Вс',
-                full: 'Воскресенье'
-            }
-        ]
-    };
+    date: new Date(),
+    years: ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021'],
+    monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    buttonMonthNames: {
+      next: 'Следующий месяц',
+      prev: 'Предыдущий месяц',
+    },
+    weekDayNames: [
+      {
+        short: 'Пн',
+        full: 'Понедельник'
+      },
+      {
+        short: 'Вт',
+        full: 'Вторник'
+      },
+      {
+        short: 'Ср',
+        full: 'Среда'
+      },
+      {
+        short: 'Чт',
+        full: 'Четверг'
+      },
+      {
+        short: 'Пт',
+        full: 'Пятница'
+      },
+      {
+        short: 'Сб',
+        full: 'Суббота'
+      },
+      {
+        short: 'Вс',
+        full: 'Воскресенье'
+      }
+    ]
+  };
 
-    state = {
-        date: this.props.date,
-        currentDate: new Date(),
-        selectedDate: null
-    };
+  state = {
+    date: this.props.date,
+    currentDate: new Date(),
+    selectedDate: null
+  };
 
-    get year() {
-        return this.state.date.getFullYear();
-    };
+  get year() {
+    return this.state.date.getFullYear();
+  };
 
-    get month() {
-        return this.state.date.getMonth();
-    };
+  get month() {
+    return this.state.date.getMonth();
+  };
 
-    get day() {
-        return this.state.date.getDate();
-    };
+  get day() {
+    return this.state.date.getDate();
+  };
 
-    handlePrevMonthButtonClick = () => {
-        const date = new Date(this.year, this.month - 1);
-        this.setState({ date });
-    };
+  handlePrevMonthButtonClick = () => {
+    const date = new Date(this.year, this.month - 1);
+    this.setState({ date });
+  };
 
-    handleNextMonthButtonClick = () => {
-        const date = new Date(this.year, this.month + 1);
-        this.setState({ date });
-    };
+  handleNextMonthButtonClick = () => {
+    const date = new Date(this.year, this.month + 1);
+    this.setState({ date });
+  };
 
-    handleSelectChange = () => {
-        const year = this.yearSelect.value;
-        const month = this.monthSelect.value;
+  handleSelectChange = () => {
+    const year = this.yearSelect.value;
+    const month = this.monthSelect.value;
 
-        const date = new Date(year, month);
-        this.setState({ date });
-    };
+    const date = new Date(year, month);
+    this.setState({ date });
+  };
 
-    handleDayClick = (date) => {
-        this.setState({ selectedDate: date });
-        this.props.onChange(date);
-    };
+  handleDayClick = (date) => {
+    this.setState({ selectedDate: date });
+    this.props.onChange(date);
+  };
 
-    render() {
-        const { years, monthNames, weekDayNames } = this.props;
-        const { currentDate, selectedDate } = this.state;
-        const monthData = calendar.getMonthData(this.year, this.month);
+  render() {
+    const { years, monthNames, weekDayNames } = this.props;
+    const { currentDate, selectedDate } = this.state;
+    const monthData = calendar.getMonthData(this.year, this.month);
 
-        return (
-            <div className="calendar card bg-dark shadow rounded-0 border-top-0">
-                <header className="card-header d-flex rounded-0">
-                    <div className="btn-group mr-3">
-                        <button
-                            className="btn btn-light"
-                            title={this.props.buttonMonthNames.prev}
-                            onClick={this.handlePrevMonthButtonClick}>
-                            
-                            ❮
-                        </button>
-                        <button
-                            className="btn btn-light"
-                            title={this.props.buttonMonthNames.next}
-                            onClick={this.handleNextMonthButtonClick}>
+    return (
+      <div className="calendar card bg-dark shadow rounded-0 border-top-0">
+        <header className="card-header d-flex rounded-0">
+          <div className="btn-group mr-3">
+            <button
+              className="btn btn-light"
+              title={this.props.buttonMonthNames.prev}
+              onClick={this.handlePrevMonthButtonClick}>
+              ❮
+            </button>
+            <button
+              className="btn btn-light"
+              title={this.props.buttonMonthNames.next}
+              onClick={this.handleNextMonthButtonClick}>
+              ❯
+            </button>
+          </div>
 
-                            ❯
-                        </button>
-                    </div>
+          <div className="input-group">
+            <select
+              value={this.month}
+              ref={element => this.monthSelect = element}
+              onChange={this.handleSelectChange}
+              className="form-control btn-light">
 
-                    <div className="input-group">
-                        <select
-                            value={this.month}
-                            ref={element => this.monthSelect = element}
-                            onChange={this.handleSelectChange}
-                            className="form-control btn-light">
+              {monthNames.map((name, index) =>
+                <option key={name} value={index}>{name}</option>
+              )}
+            </select>
+            <select
+              value={this.year}
+              ref={element => this.yearSelect = element}
+              onChange={this.handleSelectChange}
+              className="form-control btn-light">
 
-                            {monthNames.map((name, index) =>
-                                <option key={name} value={index}>{name}</option>
-                            )}
-                        </select>
-                        <select
-                            value={this.year}
-                            ref={element => this.yearSelect = element}
-                            onChange={this.handleSelectChange}
-                            className="form-control btn-light">
+              {years.map(year =>
+                <option key={year} value={year}>{year}</option>
+              )}
+            </select>
+          </div>
+        </header>
+        <section className="card-body">
+          <table className="table table-borderless text-center m-0">
+            <thead>
+              <tr>
+                {weekDayNames.map(({ short: name, full: title }) =>
+                  <th key={name} title={title} className="pb-4 text-white">{name}</th>
+                )}
+              </tr>
+            </thead>
+            <tbody>
+              {monthData.map((week, index) =>
+                <tr key={index}>
+                  {week.map((date, index) => date ?
+                    <td className="p-1" key={index}>
+                      <button
+                        className={classnames('btn h-100 w-100 rounded-0', {
+                          'text-muted': date.getMonth() !== this.month,
+                          'btn-light': calendar.isEqual(date, currentDate),
 
-                            {years.map(year =>
-                                <option key={year} value={year}>{year}</option>
-                            )}
-                        </select>
-                    </div>
-                </header>
-                <section className="card-body">
-                    <table className="table table-borderless text-center m-0">
-                        <thead>
-                            <tr>
-                                {weekDayNames.map(({ short: name, full: title }) =>
-                                    <th key={name} title={title} className="pb-4 text-white">{name}</th>
-                                )}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {monthData.map((week, index) =>
-                                <tr key={index}>
-                                    {week.map((date, index) => date ?
-                                        <td className="p-1" key={index}>
-                                            <button
-                                                className={classnames('btn h-100 w-100 rounded-0', {
-                                                    'text-muted': date.getMonth() !== this.month,
-                                                    'btn-light': calendar.isEqual(date, currentDate),
+                          'border border-light': calendar.isEqual(date, selectedDate),
+                          'border-dark': !calendar.isEqual(date, selectedDate) && !calendar.isEqual(date, currentDate)
+                        })}
+                        onClick={() => this.handleDayClick(date)}>
 
-                                                    'border border-light': calendar.isEqual(date, selectedDate),
-                                                    'border-dark': !calendar.isEqual(date, selectedDate) && !calendar.isEqual(date, currentDate)
-                                                })}
-                                                onClick={() => this.handleDayClick(date)}>
-
-                                                {date.getDate()}</button>
-                                        </td> :
-                                        <td key={index} />
-                                    )}
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </section>
-            </div>
-        );
-    }
+                        {date.getDate()}</button>
+                    </td> :
+                    <td key={index} />
+                  )}
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </section>
+      </div>
+    );
+  }
 }
